9b497a1b6cf98e5e99df07140630a001a4bc2c3e
srichandra <ch.srichandra@gmail.com>
2015-02-08 12:27:54 +0530

    Added typechecking

--- factorial.py
+++ factorial.py
@@ -1,4 +1,9 @@
-x = int(input('Give a number to print factorial: '))
+while True:
+	try:
+		x = int(input('Give a number to print factorial: '))
+		break
+	except (NameError, TypeError):
+		print "Give numbers to calculate factorial"
 fact = 1
 for i in range(1, x+1):
 	fact*=i
